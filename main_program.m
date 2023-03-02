08fc3bede74b2d0fa32504b243e11bbd3b101ff4
ssy-sheshe <525494426@qq.com>
2023-03-02 17:00:42 +0800

    main program

---
+++ main_program.m
@@ -0,0 +1,14 @@
+matric_row = 100;
+matric_BA = -generate_BA(8, matric_row);
+
+num_del = 6;
+matric_w_max = generate_matric_del(matric_BA, num_del, 'max');
+eig_w_max = eigs(matric_w_max,1,'sa');
+
+num_del = 25;
+matric_w_min = generate_matric_del(matric_BA, num_del, 'min');
+eig_w_min = eigs(matric_w_min,1,'sa');
+
+num_del = 21;
+matric_w_random = generate_matric_del(matric_BA, num_del, 'random');
+eig_w_random = eigs(matric_w_random,1,'sa');
